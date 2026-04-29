66319abb1e1f6d6397f0b37749c8dce438a3f5da
Thomaz Leventhal <thomaz.campos@fatec.sp.gov.br>
2026-04-29 08:44:45 -0300

    Adicionando diferentes inimigos

--- sword.py
+++ sword.py
@@ -5,15 +5,29 @@ import time
 def main():  
     nome_jogador = input('Olá, como você se chama? ')
     print(f'Que nome legal, {nome_jogador}!')
-    print(f'Vamos lutar!\nVocê começa.')
+    print(f'Vamos ver contra quem você ira lutar!')
+    inimigos = ['inimigo1', 'inimigo2','inimigo3']
+    escolha_inimigo = random.choice(inimigos)
+    if escolha_inimigo == 'inimigo1':
+        vida_CPU = 25
+        critmin = 15
+        critmax = 21
+    elif escolha_inimigo == 'inimigo2':
+        vida_CPU = 40
+        critmin = 1
+        critmax = 21
+    elif escolha_inimigo == 'inimigo3':
+        vida_CPU = 40
+        critmin = 1
+        critmax = 21
+         
     vida_jogador = 40
-    vida_CPU = 40
     mana_jogador = 20
     while True:
         while True:
             print(f'O que você deseja fazer? Você possuí {vida_jogador} de vida e {mana_jogador} de mana.\nSeu oponente possuí {vida_CPU} de vida.')
-            escolha1 = int(input('1- Atacar\n2- Defender\n3- Conjurar uma magia\n'))
-            if escolha1 not in [1, 2, 3]:
+            escolha1 = input('1- Atacar\n2- Defender\n3- Conjurar uma magia\n')
+            if escolha1 not in ['1', '2', '3']:
                 print('Insira apenas números válidos!')
             else:
                 break
@@ -25,7 +39,7 @@ def main():
         if escolha_CPU >= 8:
             defesa_CPU = True
 
-        if escolha1 == 1:
+        if escolha1 == '1':
             acertou = random.randrange(1,10)
             defesa = False
             if acertou <= 7:
@@ -42,10 +56,10 @@ def main():
                     print(f'Você atinge seu oponente, Causando {dano_jogador} de dano e o deixando com {vida_CPU} de vida!')
             elif acertou >= 8:
                 print('Você erra seu oponente!')
-        elif escolha1 == 2:
+        elif escolha1 == '2':
             defesa = True
             print('Você defende!')
-        elif escolha1 == 3:
+        elif escolha1 == '3':
             defesa = False
             escolha_magia = int(input('Qual magia você quer conjurar?\n1- Bola de fogo(-5 de mana)\n2- Raios(-10 de mana)\n3- Canalizar(-0 de mana)\n'))
             if escolha_magia == 1:
@@ -57,7 +71,7 @@ def main():
                     critico_jogador = random.randrange(1,21)
                     if critico_jogador == 20:
                         dano_jogador *= 2
-                        vida_CPU -= dano_jogador
+                    vida_CPU -= dano_jogador
                     if critico_jogador == 20:
                         print(f'Você conjura uma bola de fogo e atira em seu inimigo. ACERTO CRÍTICO! Você causou {dano_jogador} de dano, deixando seu oponente com {vida_CPU} de vida.')
                     else:
@@ -97,16 +111,22 @@ def main():
         if vida_CPU <= 0:
             print(f'Você venceu! Parabéns, {nome_jogador}!')
             break
+#Turno da CPU
         print('Vez da CPU!')
         if escolha_CPU <= 7:
             acertou_CPU = random.randrange(1,10)
             defesa_CPU = False
             if acertou_CPU <= 7:
                 dano_CPU = random.randrange(4,9)
+                critico_CPU = random.randrange(critmin, critmax)
+                if critico_CPU == 20:
+                    dano_CPU *= 2
+                    print(f'Ele te atingiu. ACERTO CRÍTICO! Você perde {dano_CPU} de vida.')
+                else:
+                    print(f'Ele te atingiu! Causando {dano_CPU} de dano.')
                 if defesa:
                     dano_CPU //= 2
                 vida_jogador -= dano_CPU
-                print(f'Ele te atingiu! Causando {dano_CPU} de dano.')
             elif acertou_CPU >= 8:
                 print('Ele te errou!')
         elif escolha_CPU >= 8:
